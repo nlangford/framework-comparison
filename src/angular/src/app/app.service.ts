a6f3cd3dadba747de551e557cbfb0f80a5c024cf
Neil Langford <neil.langford@live.com>
2018-04-06 00:06:58 +0100

    visuals; added delete functionality

--- src/angular/src/app/app.service.ts
+++ src/angular/src/app/app.service.ts
@@ -9,8 +9,14 @@ export class AppService {
     private http: Http
   ) { }
 
+  apiUrl = window.location.protocol + '//' + window.location.host + '/api/';
+
   getLists(){
-    return this.http.get(window.location.protocol + `//` + window.location.host + `/api/lists`).map((res:Response) => res.json());
+    return this.http.get(this.apiUrl + 'lists').map((res:Response) => res.json());
+  }
+
+  deleteList(objId) {
+    return this.http.delete(this.apiUrl + 'lists/' + objId).subscribe((ok)=>{console.log(ok)});
   }
 
 }
