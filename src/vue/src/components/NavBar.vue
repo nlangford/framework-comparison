28335a1929b478b66a94058bd6151a13ebe4e211
Neil Langford <neil.langford@live.com>
2018-04-19 23:16:26 +0100

    vue: all the things

---
+++ src/vue/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="vue">{{title}}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="react">Home <span class="sr-only">(current)</span></a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+      title: 'To Do Lists'
+    }
+  }
+}
+</script>
---
+++ src/vue/src/components/ToDoLists.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <div class="container-fluid">
+      <lists></lists>
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from './NavBar'
+import lists from './Lists'
+
+export default {
+  name: 'ToDoLists',
+  components: { navbar, lists },
+  data () {
+    return {
+      msg: 'To Do Lists'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.container-fluid{
+  margin-top: 20px;
+}
+</style>
